410089aefa14c960f517fdbdf7d6c3aa1de034c7
zday294 <zday294@students.bju.edu>
2019-03-29 19:56:45 -0400

    NUnit added

--- SerialzationUnitTests.cs
+++ SerialzationUnitTests.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
 
 namespace Binder
 {
-    public interface ISerialization
+    [TestFixture]
+    class SerialzationUnitTests
     {
-        string Serialize();
-
-        void Deserialize(string obj);
     }
 }
