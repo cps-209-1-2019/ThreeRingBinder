d34c9a3c04b65e49c474e09d1c09d0981af5a91b
cship718 <cship718@students.bju.edu>
2019-03-29 20:49:04 -0400

    Adding HighScore class and a new window. Renaming one window.

---
+++ HighScore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binder
+{
+    class HighScore
+    {
+        public int currentScore;
+        public int playerName;
+        public HighScore(int score, int name)
+        {
+            currentScore = score;
+            playerName = name;
+        }
+
+        //Loads text from high score text file
+        public void Load()
+        {
+
+        }
+
+        //Adds new name and score to `text`
+        public void AddScore(string text)
+        {
+
+        }
+
+        //Writes `text` to the high score text file
+        public void Write(string text)
+        {
+
+        }
+    }
+}
