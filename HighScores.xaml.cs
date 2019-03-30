240c65a52f73808699aaed8ecf947ff97d9dd49f
cship718 <cship718@students.bju.edu>
2019-03-29 20:36:04 -0400

    Added GUI windows

---
+++ HighScores.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Binder
+{
+    /// <summary>
+    /// Interaction logic for HighScores.xaml
+    /// </summary>
+    public partial class HighScores : Window
+    {
+        public HighScores()
+        {
+            InitializeComponent();
+        }
+    }
+}
