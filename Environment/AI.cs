3dc0fe5f14e43f9e27ca0955377bc223858ce7e1
ztapp188 <ztapp188@students.bju.edu>
2019-04-01 14:00:47 -0400

    player classes

---
+++ Environment/AI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binder.Environment
+{
+    class AI
+    {
+        public void Patrol
+        {
+
+        }
+        public void Chase
+        {
+
+        }
+    }
+}
