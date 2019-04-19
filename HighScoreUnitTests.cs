83eb3e9adfd6bad2ba402831a81e893dfd0cf07e
cship718 <cship718@students.bju.edu>
2019-04-19 14:40:22 -0400

    Improved instruction screen and pause screen

--- HighScoreUnitTests.cs
+++ HighScoreUnitTests.cs
@@ -23,6 +23,21 @@ namespace Binder
         }
 
         [Test]
+        public void HighScoreHolder_MultipleValidScores_Success()
+        {
+            HighScore score = new HighScore("450", "Dave");
+            HighScoreHolder testHolder = new HighScoreHolder();
+            testHolder.AddHighScore(score);
+            HighScore scoreTwo = new HighScore("300", "Joe");
+            testHolder.AddHighScore(scoreTwo);
+            HighScore scoreThree = new HighScore("1050", "Jim");
+            testHolder.AddHighScore(scoreThree);
+            Assert.IsTrue(testHolder.scoreList[0].CurrentScore == "1050");
+            Assert.IsTrue(testHolder.scoreList[1].PlayerName == "Dave");
+            Assert.IsTrue(testHolder.scoreList[2].CurrentScore == "300");
+        }
+
+        [Test]
         public void HighScoreHolder_Valid_LoadWriteSuccess()
         {
             HighScore score = new HighScore("600", "Joe");
